5d79da4292897eeead70b9c208e0b11eb0b48429
Justin Miller <justinrmiller@gmail.com>
2020-02-05 11:26:36 -0800

    Adding demonstration data loading

--- redis-demonstration.py
+++ redis-demonstration.py
@@ -1,18 +1,45 @@
 import click
+import redis
+import json
+from os import system, name 
+
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls')   
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
+def load_demonstration_data(redis_connection):
+    num_users = 100000
+    click.echo("Loading session data for {num_users} users.")
+    for i in range(1, num_users):
+        redis_connection.set(f"session:{i}", json.dumps({"token": f"arbitrary token for user {i}", "last_logged_in": 3600}))
 
 @click.group()
 def redis_demonstration():
     pass
 
 @redis_demonstration.command()
-def cmd1():
-    '''Command on redis_demonstration'''
-    click.echo('redis_demonstration cmd1')
-
-@redis_demonstration.command()
-def cmd2():
-    '''Command on redis_demonstration'''
-    click.echo('redis_demonstration cmd2')
+@click.option('--rediss', prompt='Rediss Connection String', help='This is the rediss:// connection string.')
+def demo(rediss):
+    exit = False
+    while (exit == False):
+        clear()
+        redis_pool = redis.ConnectionPool.from_url(url=rediss)
+        redis_conn = redis.Redis(connection_pool=redis_pool)
+        click.echo(f'Connected to redis instance: {rediss}')
+        click.echo("Options:\n" + 
+            "1. Load demonstration data\n"
+            "10. Exit\n"
+        )
+        option = click.prompt('Please choose an option: ', type=int)
+        if option == 1: 
+            load_demonstration_data(redis_conn)
+            click.confirm('Data loaded. Hit enter to return to main menu.')
+        if option == 10:
+            exit = True
 
 if __name__ == '__main__':
     redis_demonstration()
